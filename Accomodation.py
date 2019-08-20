177966414d116a3a2e5d46b0ae5cc8332d68bfa8
andrea9888 <50518294+andrea9888@users.noreply.github.com>
2019-08-20 16:04:16 +0200

    Deployed to Heroku

--- Accomodation.py
+++ Accomodation.py
@@ -1,6 +1,6 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+myclient = pymongo.MongoClient("mongodb+srv://andrea9888:andrea9888@cluster0-t8pwd.mongodb.net/test?retryWrites=true&w=majority")
 mydb = myclient["Realitica"]
 if not "Accomodation" in mydb.list_collection_names():
     mycol = mydb.create_collection("Accomodation")
--- first.py
+++ first.py
@@ -3,53 +3,54 @@ import requests
 from Accomodation import mycol
 import datetime
 import time
+import os
 from flask import Flask
 
 
-#app = Flask(__name__)
+app = Flask(__name__)
 def str_to_date(string):
     date_string = string.replace(" ", "/").replace(",", "")
     return datetime.datetime.strptime(date_string, "%m/%b/%Y")
 
-def getTagContent(htmlString,tag):
-    konacna_lista_sadrzaja=[]
-    def ima_li_taga(htmlString,tag):
+def get_tag_content(htmlString,tag):
+    list_of_content=[]
+    def tag_exsists(htmlString,tag):
         tag_1='<'+tag
-        brojac=0 #broji da li se svi brojevi u nazivu taga poklapaju
+        count1=0 
         for (count,elem) in enumerate(htmlString):
             if elem==tag_1[0]:
                 if htmlString[count+1]==tag_1[1]:
-                    brojac=1 
+                    count1=1 
                     for count2 in range(1,len(tag)):
                         if htmlString[count+1+count2]==tag[count2]:
-                            brojac=brojac+1
-                    if brojac==len(tag):
+                            count1=count1+1
+                    if count1==len(tag):
                         return True
                     
     while 1:
-        if (ima_li_taga(htmlString,tag))==True:
+        if (tag_exsists(htmlString,tag))==True:
             tag_1='<'+tag
             tag_2=tag+'>'
             lista1=htmlString.partition(tag_1)
             lista2=lista1[2].partition(tag_2)
-            konacna_lista_sadrzaja.append(lista2[0])
+            list_of_content.append(lista2[0])
             htmlString=lista2[2]
         else:
             break
-    konacna_lista_sadrzaja_bez_oznaka=[]
-    for elem in konacna_lista_sadrzaja:
-        pocetak=elem.find(">")
-        kraj=elem.find('</')
-        konacna_lista_sadrzaja_bez_oznaka.append(elem[pocetak+1:kraj].replace('\n', '').lstrip().rstrip())
-    return konacna_lista_sadrzaja_bez_oznaka
-#@app.route("/")
+    list_of_content_final=[]
+    for elem in list_of_content:
+        start=elem.find(">")
+        end=elem.find('</')
+        list_of_content_final.append(elem[start+1:end].replace('\n', '').lstrip().rstrip())
+    return list_of_content_final
+@app.route("/")
 def get_content(content_name, link1, a):
     #print(str(link1).split("strong>"))
     html_list = str(link1).split("strong>")
     last_change=html_list.index(content_name+'</')
     if content_name == "Oglasio":
         string = html_list[last_change+1].split('\n')[0].strip(': ') + "s"
-        return getTagContent(string.split('\n')[0].strip(': '), "a")[0]
+        return get_tag_content(string.split('\n')[0].strip(': '), "a")[0]
     if content_name == "Opis":
         if "<br/>" in html_list[last_change+1]:
             return html_list[last_change+1].split('\n')[0].strip(': ').split("<br/>")[0]
@@ -97,6 +98,7 @@ while 1:
         html = BeautifulSoup(source,'lxml')
         link = html.find_all('div', class_ = 'thumb_div')
         for elem in link:
+            time.sleep(2)
             current_ad = {}
             url=elem.a['href']
             source1=requests.get(url).text
@@ -137,12 +139,12 @@ while 1:
             link1 = html1.find_all('div', id= 'listing_body')
             b = "<br/><"
             
-            list_of_fields = getTagContent(str(link1), "strong")
+            list_of_fields = get_tag_content(str(link1), "strong")
             
             required=["Lokacija", "Opis", "Oglasio"]
             phone=['Mobitel', "Telefon", "ME", "Mobilni"]
             phone_exsists=0
-            if  set(required).issubset(list_of_fields) and getTagContent(str(link1), "h2")[0]:
+            if  set(required).issubset(list_of_fields) and get_tag_content(str(link1), "h2")[0]:
                 for elem in phone: 
                     if elem in list_of_fields:
                             phone_exsists=1
@@ -157,7 +159,7 @@ while 1:
                 continue
             unwanted=["Zadnja Promjena", "Oglas Broj", "Tags", "Više detalja na"]
             list_of_fields=[elem for elem in list_of_fields if elem not in unwanted]
-            current_ad.update({"naslov":getTagContent(str(link1), "h2")[0]})
+            current_ad.update({"naslov":get_tag_content(str(link1), "h2")[0]})
             for elem in list_of_fields:
                 a = {elem.lower() : get_content(elem,link1, b)}
                 current_ad.update(a)
@@ -178,7 +180,7 @@ while 1:
 
         
 
-            time.sleep(2)
+            
             
           
                 
@@ -187,6 +189,8 @@ while 1:
         
     except Exception as e :
         print(str(e))
-        print("Kraj")    
+        print("end")    
         break
-    
+if __name__=="__main__" :
+    port= int(os.environ.get("PORT", 17995))
+    app.run(host="0.0.0.0", port=port) 
