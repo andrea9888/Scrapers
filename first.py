f411d4f25f7bc3e129e5af330e1d7344fcaf76f4
filippilif155 <50518281+filippilif155@users.noreply.github.com>
2019-08-20 18:37:53 +0200

    Add files via upload

--- first.py
+++ first.py
@@ -3,53 +3,63 @@ import requests
 from Accomodation import mycol
 import datetime
 import time
+import os
 from flask import Flask
 
 
 #app = Flask(__name__)
+
 def str_to_date(string):
     date_string = string.replace(" ", "/").replace(",", "")
     return datetime.datetime.strptime(date_string, "%m/%b/%Y")
 
-def getTagContent(htmlString,tag):
-    konacna_lista_sadrzaja=[]
-    def ima_li_taga(htmlString,tag):
+def get_tag_content(htmlString,tag):
+    list_of_content=[]
+    def tag_exsists(htmlString,tag):
         tag_1='<'+tag
-        brojac=0 #broji da li se svi brojevi u nazivu taga poklapaju
+        count1=0 
         for (count,elem) in enumerate(htmlString):
             if elem==tag_1[0]:
                 if htmlString[count+1]==tag_1[1]:
-                    brojac=1 
+                    count1=1 
                     for count2 in range(1,len(tag)):
                         if htmlString[count+1+count2]==tag[count2]:
-                            brojac=brojac+1
-                    if brojac==len(tag):
+                            count1=count1+1
+                    if count1==len(tag):
                         return True
                     
     while 1:
-        if (ima_li_taga(htmlString,tag))==True:
+        if (tag_exsists(htmlString,tag))==True:
             tag_1='<'+tag
             tag_2=tag+'>'
             lista1=htmlString.partition(tag_1)
             lista2=lista1[2].partition(tag_2)
-            konacna_lista_sadrzaja.append(lista2[0])
+            list_of_content.append(lista2[0])
             htmlString=lista2[2]
         else:
             break
-    konacna_lista_sadrzaja_bez_oznaka=[]
-    for elem in konacna_lista_sadrzaja:
-        pocetak=elem.find(">")
-        kraj=elem.find('</')
-        konacna_lista_sadrzaja_bez_oznaka.append(elem[pocetak+1:kraj].replace('\n', '').lstrip().rstrip())
-    return konacna_lista_sadrzaja_bez_oznaka
+    list_of_content_final=[]
+    for elem in list_of_content:
+        start=elem.find(">")
+        end=elem.find('</')
+        list_of_content_final.append(elem[start+1:end].replace('\n', '').lstrip().rstrip())
+    return list_of_content_final
 #@app.route("/")
 def get_content(content_name, link1, a):
     #print(str(link1).split("strong>"))
     html_list = str(link1).split("strong>")
-    last_change=html_list.index(content_name+'</')
+    try:
+        last_change=html_list.index(content_name+'</')
+    except:
+        return None
+    if content_name == "Više detalja na":
+        if "href" in html_list[last_change + 1]:
+            position=html_list[last_change + 1].split("href=")[1].index("onclick")
+            return html_list[last_change + 1].split("href=")[1][1:position-2]
+
     if content_name == "Oglasio":
         string = html_list[last_change+1].split('\n')[0].strip(': ') + "s"
-        return getTagContent(string.split('\n')[0].strip(': '), "a")[0]
+        return get_tag_content(string.split('\n')[0].strip(': '), "a")[0]
     if content_name == "Opis":
         if "<br/>" in html_list[last_change+1]:
             return html_list[last_change+1].split('\n')[0].strip(': ').split("<br/>")[0]
@@ -72,7 +82,7 @@ def get_content(content_name, link1, a):
     if content_name in ["Novogradnja", "Klima Uređaj"]:
         return True
     if a!="":
-        if content_name=="Stambena Površina":
+        if content_name=="Stambena Površina" or content_name=="Zemljište":
             return int(html_list[last_change+1].split('\n')[0].strip(': ')[:-len(a)].split("<font")[0].strip("m"))
         if content_name == "Spavaćih Soba" or content_name == "Kupatila" or content_name == "Od Mora (m)" or content_name == "Parking Mjesta" :
             if content_name == "Parking Mjesta":
@@ -81,8 +91,6 @@ def get_content(content_name, link1, a):
             return int(html_list[last_change+1].split('\n')[0].strip(': ')[:-len(a)])
         elif content_name == "Cijena":
             return float(html_list[last_change+1].split('\n')[0].strip(': ')[:-len(a)].strip("€"))
-        elif content_name == "Zemljište":
-            return html_list[last_change+1].split('\n')[0].strip(': ')[:-len(a)]
         else: 
             return html_list[last_change+1].split('\n')[0].strip(': ')[:-len(a)]
     else:
@@ -97,9 +105,10 @@ while 1:
         html = BeautifulSoup(source,'lxml')
         link = html.find_all('div', class_ = 'thumb_div')
         for elem in link:
+            time.sleep(2)
             current_ad = {}
             url=elem.a['href']
-            source1=requests.get(url).text
+            source1=requests.get("https://www.realitica.com/hr/listing/1979846").text
             html1 = BeautifulSoup(source1,'lxml')
             link1 = html1.find_all('div', style= 'clear:both;')
             a = ""
@@ -107,22 +116,24 @@ while 1:
                 current_ad = {"zadnja promjena" : str_to_date(get_content("Zadnja Promjena",link1, a))}
                 current_ad.update({"oglas broj" : int( get_content("Oglas Broj",link1, a))})
             except:
-                print("Ad does not contain required fields!")
                 continue
             help = 0
             for ad in mycol.find():
                 try:
                     if ad["oglas broj"] == current_ad["oglas broj"]:
                         if ad["zadnja promjena"] == current_ad["zadnja promjena"]:
+                            print("Ad already exsists!")
                             help = 2
-                            break
+                            break  
                         help = 1
                         break
                 except:
                     continue
             if help == 2:
-                print("Ad already exsists!")
                 continue
+            print("------0")
+            #print(get_content("Više detalja na",link1, a))
+            print("--------")
             link1 = html1.find('div', id= 'rea_blueimp')
             list_of_images=[]
             help_list=str(link1).split("\n")
@@ -135,14 +146,17 @@ while 1:
                 print("Ad does not contain required fields!")
                 continue 
             link1 = html1.find_all('div', id= 'listing_body')
+            print("----")
+
             b = "<br/><"
             
-            list_of_fields = getTagContent(str(link1), "strong")
+            list_of_fields = get_tag_content(str(link1), "strong")
+            print(link1)
             
             required=["Lokacija", "Opis", "Oglasio"]
             phone=['Mobitel', "Telefon", "ME", "Mobilni"]
             phone_exsists=0
-            if  set(required).issubset(list_of_fields) and getTagContent(str(link1), "h2")[0]:
+            if  set(required).issubset(list_of_fields) and get_tag_content(str(link1), "h2")[0]:
                 for elem in phone: 
                     if elem in list_of_fields:
                             phone_exsists=1
@@ -157,11 +171,11 @@ while 1:
                 continue
             unwanted=["Zadnja Promjena", "Oglas Broj", "Tags", "Više detalja na"]
             list_of_fields=[elem for elem in list_of_fields if elem not in unwanted]
-            current_ad.update({"naslov":getTagContent(str(link1), "h2")[0]})
+            current_ad.update({"naslov":get_tag_content(str(link1), "h2")[0]})
             for elem in list_of_fields:
                 a = {elem.lower() : get_content(elem,link1, b)}
                 current_ad.update(a)
-            list_of_optional = ["vrsta", "pordučje", "spavaćih soba", "kupatila", "cijena", "stambena površina", "parking mjesta", "zemljište", "od mora (m)", "klima uređaj", "novogradnja", "web stranica"]
+            list_of_optional = ["vrsta", "pordučje", "spavaćih soba", "kupatila", "cijena", "stambena površina", "parking mjesta", "zemljište", "od mora (m)", "klima uređaj", "novogradnja", "više detalja na"]
             for elem in list_of_optional:
                 if elem not in current_ad.keys():
                     current_ad.update({elem:"Not stated" })
@@ -175,18 +189,24 @@ while 1:
                     print("Successfully updated!")
             except Exception as e:
                 print(str(e))
-
-        
-
-            time.sleep(2)
-            
-          
-                
+    
 
         i=i+1
         
     except Exception as e :
         print(str(e))
-        print("Kraj")    
+        print("end")    
         break
-    
+
+
+def get_web():
+    list_of_sites = []
+    for ad in mycol:
+        if "više detalja na" in ad.keys():
+            list_of_sites.append(ad)
+    return ad
+'''
+if __name__=="__main__" :
+    port= int(os.environ.get("PORT", 17995))
+    app.run(host="0.0.0.0", port=port)
+    ''' 
